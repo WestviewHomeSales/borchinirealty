05c791e44ed58aa710cdcc3a5d19e627e57ea0c4
WestviewHomeSales <WestviewHomeSales@gmail.com>
2025-06-29 15:29:41 -0600

    Update $500,000+ button to use HTTPS

--- src/components/HomeSearches.tsx
+++ src/components/HomeSearches.tsx
@@ -143,6 +143,12 @@ const HomeSearches = () => {
       return;
     }
     
+    // Special handling for "$500,000+" - redirect to external IDX site
+    if (filter.priceRange === '500000+') {
+      window.open('https://borchinirealty.idxbroker.com/i/500000-1000000', '_blank', 'noopener,noreferrer');
+      return;
+    }
+    
     if (filter.type === 'price-overview') {
       // Show price overview
       navigate('/solivita-home-searches');
